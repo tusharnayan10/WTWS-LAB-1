fd76ed302a026a85b787fb48826a3526c14af322
Tushar Nayan <tusharnayan2610@gmail.com>
2021-08-30 11:48:28 +0530

    publish new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,14 @@ $('.message .close')
             .transition('fade')
             ;
     });
+
+// Init AOS
+function aos_init() {
+    AOS.init({
+        duration: 1000,
+        once: true
+    });
+}
+$(window).on('load', function () {
+    aos_init();
+});
